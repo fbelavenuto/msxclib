b858058c42fd3ce4ba316afb64ccd1bd2c0c2571
Fabio Belavenuto <belavenuto@gmail.com>
2018-12-11 16:23:01 -0200

    Formating

--- lib/bios.s
+++ lib/bios.s
@@ -6,12 +6,12 @@ CALSLT	= 0x001C
 ENASLT	= 0x0024
 RESET	= 0x0000
 KILBUF	= 0x0156
-RAMAD0	= 0xF341				; slotid DOS ram page 0
-RAMAD1	= 0xF342				; slotid DOS ram page 1
-RAMAD2	= 0xF343				; slotid DOS ram page 2
-RAMAD3	= 0xF344				; slotid DOS ram page 3
-EXPTBL	= 0xFCC1				; slotids
-DSKSLT	= 0xF348				; slotid DOS diskrom
+RAMAD0	= 0xF341		; slotid DOS ram page 0
+RAMAD1	= 0xF342		; slotid DOS ram page 1
+RAMAD2	= 0xF343		; slotid DOS ram page 2
+RAMAD3	= 0xF344		; slotid DOS ram page 3
+EXPTBL	= 0xFCC1		; slotids
+DSKSLT	= 0xF348		; slotid DOS diskrom
 
 	.area	_CODE
 
@@ -20,50 +20,50 @@ DSKSLT	= 0xF348				; slotid DOS diskrom
 ; void clearKeyBuf(void)
 ;-------------------------------------------------------------------------------
 _clearKeyBuf::
-	ld		ix, #KILBUF
-	ld		iy, (EXPTBL-1)
-	jp		CALSLT
+	ld	ix, #KILBUF
+	ld	iy, (EXPTBL-1)
+	jp	CALSLT
 
 ;-------------------------------------------------------------------------------
 ; void putRamFrame1(void)
 ;-------------------------------------------------------------------------------
 _putRamFrame1::
-	ld		a, (RAMAD1)
-	ld		h, #0x40
-	jp		ENASLT
+	ld	a, (RAMAD1)
+	ld	h, #0x40
+	jp	ENASLT
 
 ;-------------------------------------------------------------------------------
 ; void putRamFrame2(void)
 ;-------------------------------------------------------------------------------
 _putRamFrame2::
-	ld		a, (RAMAD2)
-	ld		h, #0x80
-	jp		ENASLT
+	ld	a, (RAMAD2)
+	ld	h, #0x80
+	jp	ENASLT
 
 ;-------------------------------------------------------------------------------
 ; void putSlotFrame1(unsigned char slot)
 ;-------------------------------------------------------------------------------
 _putSlotFrame1::
-	ld		iy, #0
-	add		iy, sp
-	ld		a, 2(iy)
-	ld		h, #0x40
+	ld	iy, #0
+	add	iy, sp
+	ld	a, 2(iy)
+	ld	h, #0x40
 	jp		ENASLT
 
 ;-------------------------------------------------------------------------------
 ; void putSlotFrame2(unsigned char slot)
 ;-------------------------------------------------------------------------------
 _putSlotFrame2::
-	ld		iy, #0
-	add		iy, sp
-	ld		a, 2(iy)
-	ld		h, #0x80
-	jp		ENASLT
+	ld	iy, #0
+	add	iy, sp
+	ld	a, 2(iy)
+	ld	h, #0x80
+	jp	ENASLT
 
 ;-------------------------------------------------------------------------------
 ; void resetSystem(void)
 ;-------------------------------------------------------------------------------
 _resetSystem::
-	ld		ix, #RESET
-	ld		iy, (EXPTBL-1)
-	jp		CALSLT
+	ld	ix, #RESET
+	ld	iy, (EXPTBL-1)
+	jp	CALSLT
--- lib/mapper.s
+++ lib/mapper.s
@@ -27,94 +27,94 @@ HOKVLD	= 0xFB20
 ; unsigned char mpInit(void)
 ;-------------------------------------------------------------------------------
 _mpInit::
-	ld		a, (HOKVLD)
-	bit		0, a
-	jp z,	.error1
-	ld		a, #0
-	ld		de, #0x0401
+	ld	a, (HOKVLD)
+	bit	0, a
+	jp	z, .error1
+	ld	a, #0
+	ld	de, #0x0401	; Device 4 Function 1
 	call	EXTBIO
-	and		a
-	jp z,	.error1
-	ld		(_mpSlotAddr), a
-	ld		(_mpVars), hl
-	ld		a, #0
-	ld		de, #0x0402
+	and	a
+	jp	z, .error1
+	ld	(_mpSlotAddr), a
+	ld	(_mpVars), hl
+	ld	a, #0
+	ld	de, #0x0402	; Device 4 Function 2
 	call	EXTBIO
-	and		a
-	jp z,	.error1
-	ld		a, b
-	ld		(_mpSlotNum), a
-	ld		(mpFuncs), hl
-	ld		de, #mpfAllocSeg		; copy jumptable
-	ld		bc, #16 * 3
+	and	a
+	jp	z, .error1
+	ld	a, b
+	ld	(_mpSlotNum), a
+	ld	(mpFuncs), hl
+	ld	de, #mpfAllocSeg	; copy jumptable
+	ld	bc, #16 * 3
 	ldir
-	ld		l, #0
+	ld	l, #0
 .exit1:
 	ret
 .error1:
-	ld		l, #1
+	ld	l, #1
 	ret
 
 ;-------------------------------------------------------------------------------
 ; unsigned char numMapperPages(void)
 ;-------------------------------------------------------------------------------
 _numMapperPages::
-	ld		c, #0xFE
-	in		a, (c)
+	ld	c, #0xFE
+	in	a, (c)
 	push	af
-	ld		de, #0x0005					; D = number of pages, E = start page
-	ld		hl, #0x8000					; Address os frame 2
-	ld		a, #4
-	out		(c), a						; Set page 4
-	ld		b, (hl)						; Read actual byte and save it
-	ld		a, #0xAA
-	ld		(hl), a						; Put 0xAA
+	ld	de, #0x0005	; D = number of pages, E = start page
+	ld	hl, #0x8000	; Address os frame 2
+	ld	a, #4
+	out	(c), a		; Set page 4
+	ld	b, (hl)		; Read actual byte and save it
+	ld	a, #0xAA
+	ld	(hl), a		; Put 0xAA
 nmp_loop:
-	out		(c), e
-	ld		a, (hl)						; Read byte from another page
-	cp		#0xAA						; Is the same?
-	jr z, 	nmp_same					; Yes, test if it is the #4 page
-	inc		d							; No, is a valid page, increment
+	out	(c), e
+	ld	a, (hl)		; Read byte from another page
+	cp	#0xAA		; Is the same?
+	jr	z, nmp_same	; Yes, test if it is the #4 page
+	inc	d		; No, is a valid page, increment
 nmp_next:
-	inc		e							; Test next page
-	jr nz,	nmp_loop
-	jr		nmp_exit
+	inc	e		; Test next page
+	jr	nz, nmp_loop
+	jr	nmp_exit
 nmp_same:
-	cpl									; Invert all bits
-	ld		(hl), a
+	cpl			; Invert all bits
+	ld	(hl), a
 	push	af
-	ld		a, #4
-	out		(c), a						; Set page 4
-	pop		af
-	cp		(hl)						; Is the same
-	jr z,	nmp_exit2					; Yes, the pages overlapped
-	out		(c), e
+	ld	a, #4
+	out	(c), a		; Set page 4
+	pop	af
+	cp	(hl)		; Is the same
+	jr	z, nmp_exit2	; Yes, the pages overlapped
+	out	(c), e
 	cpl
-	ld		(hl), a						; No, restore RAM byte
-	jr		nmp_next					; Next
+	ld	(hl), a		; No, restore RAM byte
+	jr	nmp_next	; Next
 nmp_exit:
-	ld		a, #4
-	out		(c), a						; Set page 4
+	ld	a, #4
+	out	(c), a		; Set page 4
 nmp_exit2:
-	ld		(hl), b						; Restore original value
-	pop		af
-	out		(c), a
-	ld		a, d
-	ld		b, #3						; Exclude initial 64K (last page-3)
-	sub		b
-	ld		l, a
+	ld	(hl), b		; Restore original value
+	pop	af
+	out	(c), a
+	ld	a, d
+	ld	b, #3		; Exclude initial 64K (last page-3)
+	sub	b
+	ld	l, a
 	ret
 
 ;-------------------------------------------------------------------------------
 ; unsigned char allocUserSegment(void)
 ;-------------------------------------------------------------------------------
 _allocUserSegment::
-	xor		a
-	ld		b, a
+	xor	a
+	ld	b, a
 	call	mpfAllocSeg
-	ld		l, #0
-	jr c,	.exit2
-	ld		l, a
+	ld	l, #0
+	jr	c, .exit2
+	ld	l, a
 .exit2:
 	ret
 
@@ -122,13 +122,13 @@ _allocUserSegment::
 ; unsigned char allocSysSegment(void)
 ;-------------------------------------------------------------------------------
 _allocSysSegment::
-	xor		a
-	ld		b, a
-	inc		a
+	xor	a
+	ld	b, a
+	inc	a
 	call	mpfAllocSeg
-	ld		l, #0
-	jr c,	.exit3
-	ld		l, a
+	ld	l, #0
+	jr	c, .exit3
+	ld	l, a
 .exit3:
 	ret
 
@@ -136,15 +136,15 @@ _allocSysSegment::
 ; unsigned char freeSegment(unsigned char segm)
 ;-------------------------------------------------------------------------------
 _freeSegment::
-	xor		a
-	ld		b, a
-	ld		iy, #0
-	add		iy, sp
-	ld		a, 2(iy)
+	xor	a
+	ld	b, a
+	ld	iy, #0
+	add	iy, sp
+	ld	a, 2(iy)
 	call	mpfFreeSeg
-	ld		l, #0
-	jr c,	.exit4
-	ld		l, #1
+	ld	l, #0
+	jr	c, .exit4
+	ld	l, #1
 .exit4:
 	ret
 
@@ -153,7 +153,7 @@ _freeSegment::
 ;-------------------------------------------------------------------------------
 _getCurSegFrame1::
 	call	mpfGetP1
-	ld		l, a
+	ld	l, a
 	ret
 
 ;-------------------------------------------------------------------------------
@@ -161,16 +161,16 @@ _getCurSegFrame1::
 ;-------------------------------------------------------------------------------
 _getCurSegFrame2::
 	call	mpfGetP2
-	ld		l, a
+	ld	l, a
 	ret
 
 ;-------------------------------------------------------------------------------
 ; void putSegFrame1(unsigned char segm)
 ;-------------------------------------------------------------------------------
 _putSegFrame1::
-	ld		iy, #0
-	add		iy, sp
-	ld		a, 2(iy)
+	ld	iy, #0
+	add	iy, sp
+	ld	a, 2(iy)
 	call	mpfPutP1
 	ret
 
@@ -178,18 +178,18 @@ _putSegFrame1::
 ; void putSegFrame2(unsigned char segm)
 ;-------------------------------------------------------------------------------
 _putSegFrame2::
-	ld		iy, #0
-	add		iy, sp
-	ld		a, 2(iy)
+	ld	iy, #0
+	add	iy, sp
+	ld	a, 2(iy)
 	call	mpfPutP2
 	ret
 
 ;-------------------------------------------------------------------------------
 myAlloc:
-	ld		hl, #initSeg
-	ld		a, (hl)
-	inc		(hl)
-	ld		l, a
+	ld	hl, #initSeg
+	ld	a, (hl)
+	inc	(hl)
+	ld	l, a
 	ret
 
 ;-------------------------------------------------------------------------------
@@ -197,59 +197,59 @@ myAlloc:
 mpfAllocSeg:
 	call	myAlloc
 mpfFreeSeg:
-	.db		0xC9,0xC9,0xC9
+	.db	0xC9,0xC9,0xC9
 mpfReadSeg:
-	.db		0xC9,0xC9,0xC9
+	.db	0xC9,0xC9,0xC9
 mpfWriteSeg:
-	.db		0xC9,0xC9,0xC9
+	.db	0xC9,0xC9,0xC9
 mpfCallSeg:
-	.db		0xC9,0xC9,0xC9
+	.db	0xC9,0xC9,0xC9
 mpfCallS:
-	.db		0xC9,0xC9,0xC9
+	.db	0xC9,0xC9,0xC9
 mpfPutPh:
-	.db		0xC9,0xC9,0xC9
+	.db	0xC9,0xC9,0xC9
 mpfGetPh:
-	.db		0xC9,0xC9,0xC9
+	.db	0xC9,0xC9,0xC9
 mpfPutP0:
-	out		(#0xFC), a
+	out	(#0xFC), a
 	ret
 mpfGetP0:
-	in		a, (#0xFC)
+	in	a, (#0xFC)
 	ret
 mpfPutP1:
-	out		(#0xFD), a
+	out	(#0xFD), a
 	ret
 mpfGetP1:
-	in		a, (#0xFD)
+	in	a, (#0xFD)
 	ret
 mpfPutP2:
-	out		(#0xFE), a
+	out	(#0xFE), a
 	ret
 mpfGetP2:
-	in		a, (#0xFE)
+	in	a, (#0xFE)
 	ret
 empty:
-	.db		0xC9,0xC9,0xC9
+	.db	0xC9,0xC9,0xC9
 mpfGetP3:
-	in		a, (#0xFF)
+	in	a, (#0xFF)
 	ret
 
 ;mpfAllocSeg	= 0x00	; Allocate a 16k segment.
-;mpfFreeSeg		= 0x03	; Free a 16k segment.
-;mpfReadSeg		= 0x06	; Read byte from address A:HL to A.
+;mpfFreeSeg	= 0x03	; Free a 16k segment.
+;mpfReadSeg	= 0x06	; Read byte from address A:HL to A.
 ;mpfWriteSeg	= 0x09	; Write byte from E to address A:HL.
-;mpfCallSeg		= 0x0C	; Inter-segment call.  Address in IYh:IX
-;mpfCallS		= 0x0F	; Inter-segment call.  Address in line after the call instruction.
-;mpfPutPh		= 0x12	; Put segment into page (HL).
-;mpfGetPh		= 0x15	; Get current segment for page (HL)
-;mpfPutP0		= 0x18	; Put segment into page 0.
-;mpfGetP0		= 0x1B	; Get current segment for page 0.
-;mpfPutP1		= 0x1E	; Put segment into page 1.
-;mpfGetP1		= 0x21	; Get current segment for page 1.
-;mpfPutP2		= 0x24	; Put segment into page 2.
-;mpfGetP2		= 0x27	; Get current segment for page 2.
+;mpfCallSeg	= 0x0C	; Inter-segment call.  Address in IYh:IX
+;mpfCallS	= 0x0F	; Inter-segment call.  Address in line after the call instruction.
+;mpfPutPh	= 0x12	; Put segment into page (HL).
+;mpfGetPh	= 0x15	; Get current segment for page (HL)
+;mpfPutP0	= 0x18	; Put segment into page 0.
+;mpfGetP0	= 0x1B	; Get current segment for page 0.
+;mpfPutP1	= 0x1E	; Put segment into page 1.
+;mpfGetP1	= 0x21	; Get current segment for page 1.
+;mpfPutP2	= 0x24	; Put segment into page 2.
+;mpfGetP2	= 0x27	; Get current segment for page 2.
 ;;
-;mpfGetP3		= 0x2D	; Get current segment for page 3.
+;mpfGetP3	= 0x2D	; Get current segment for page 3.
 
 ;ALL_SEG - Parameters:   A=0 => allocate user segment
 ;                        A=1 => allocate system segment
@@ -323,13 +323,13 @@ mpfGetP3:
 	.area	_DATA
 
 _mpSlotAddr::
-	.ds		1
+	.ds	1
 _mpSlotNum::
-	.ds		1
+	.ds	1
 _mpVars::
-	.ds		2
+	.ds	2
 mpFuncs:
-	.ds		2
+	.ds	2
 initSeg:
-	.db		4
+	.db	4
 
